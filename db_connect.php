374f4e3845237a82f11f92eed711d738cbbc85f4
Manasvi Arora <122682063+Mansia13@users.noreply.github.com>
2025-08-21 14:54:04 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";  // Change this if your DB user is different
-$password = "";      // Add your DB password if needed
-$dbname = "travel_saathi";  // Use the correct database name
+$host =  "b5ilusf2okfawox3z3fd-mysql.services.clever-cloud.com";  // from Clever Cloud
+$user = "upgyhf5dekgz1hun";  // username
+$pass = "aCn2K7kE47jwzCIIDRHn";  // password
+$db   = "b5ilusf2okfawox3z3fd";   // database name
+$port = 3306;         // port
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+
